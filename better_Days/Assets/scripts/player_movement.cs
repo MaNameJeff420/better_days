0effbe9d92dc29e32ec01612ee1d985a8a6d96f5
boxwalk <150533452+boxwalk@users.noreply.github.com>
2023-11-12 12:58:46 +0000

    basic movement
    theo i love you uwu

---
+++ better_Days/Assets/scripts/player_movement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    [SerializeField] private int player_number;
+
+    private bool move_right_pressed;
+    private bool move_left_pressed;
+    private bool jump_pressed;
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jump_height;
+    [SerializeField] private Transform ground_check;
+    [SerializeField] private float ground_check_radius;
+    [SerializeField] private LayerMask ground_mask;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        gather_input();
+        playermovement();
+    }
+
+    void gather_input()
+    {
+        if(player_number == 1)
+        {
+            move_left_pressed = Input.GetKey(KeyCode.A);
+            move_right_pressed = Input.GetKey(KeyCode.D);
+            jump_pressed = Input.GetKeyDown(KeyCode.W);
+        }else
+        {
+            move_left_pressed = Input.GetKey(KeyCode.LeftArrow);
+            move_right_pressed = Input.GetKey(KeyCode.RightArrow);
+            jump_pressed = Input.GetKeyDown(KeyCode.UpArrow);
+        }
+    }
+
+    void playermovement()
+    {
+        //horizontal movement
+        if (move_left_pressed)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }else if (move_right_pressed)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //vertical movement
+        if (jump_pressed && is_grounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_height);
+        }
+    }
+
+    //grounded check
+    bool is_grounded()
+    {
+        return Physics2D.OverlapCircle(ground_check.position, ground_check_radius, ground_mask);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(ground_check.position, ground_check_radius);
+    }
+}
